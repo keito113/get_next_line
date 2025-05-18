651f4ce504b0a87fcab9a196c05b08d0178456d1
Keito Abe <keitabe@c4r3s14.42tokyo.jp>
2025-05-18 14:26:04 +0900

    mada_dekitenaiyo

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: keitabe <keitabe@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/17 09:27:06 by keitabe           #+#    #+#             */
-/*   Updated: 2025/05/17 14:22:59 by keitabe          ###   ########.fr       */
+/*   Updated: 2025/05/18 14:22:28 by keitabe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,24 @@
 
 char	*get_next_line(int fd)
 {
+	static char	*store;
+	char		buf[BUFFER_SIZE + 1];
+	char		*line;
+	ssize_t		byt_read;
+
+	if (BUFFER_SIZE <= 0 || fd < 0)
+		return (NULL);
+	byt_read = 0;
+	while (gnl_newline_index(store) < 0 && (byt_read = read(fd, buf,
+				BUFFER_SIZE)) > 0)
+	{
+		buf[byt_read] = '\0';
+		if (!(store = gnl_strjoin_free(store, buf)))
+			return (NULL);
+	}
+	if (byt_read < 0)
+		return (free(store), store = NULL, NULL);
+	line = gnl_clip_line(store);
+	store = gnl_get_remainder(store);
+	return (line);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: keitabe <keitabe@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/17 09:27:01 by keitabe           #+#    #+#             */
-/*   Updated: 2025/05/17 15:22:48 by keitabe          ###   ########.fr       */
+/*   Updated: 2025/05/18 14:22:12 by keitabe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ size_t	gnl_strlen(const char *str)
 	return (i);
 }
 
-char	*gnl_strjoin_free(char *stor, const char *buf)
+char	*gnl_strjoin_free(char *store, const char *buf)
 {
 	size_t	len1;
 	size_t	len2;
@@ -32,69 +32,69 @@ char	*gnl_strjoin_free(char *stor, const char *buf)
 	size_t	j;
 	char	*new_str;
 
-	len1 = gnl_strlen(stor);
+	len1 = gnl_strlen(store);
 	len2 = gnl_strlen(buf);
 	if (len1 > SIZE_MAX - len2 - 1)
-		return (free(stor), NULL);
+		return (free(store), NULL);
 	new_str = malloc(len1 + len2 + 1);
 	if (!new_str)
-		return (free(stor), NULL);
+		return (free(store), NULL);
 	i = 0;
 	while (i < len1)
-		new_str[i] = stor[i++];
+		new_str[i] = store[i++];
 	j = 0;
 	while (j < len2)
 		new_str[i + j] = buf[j++];
 	new_str[i + j] = '\0';
-	free(stor);
+	free(store);
 	return (new_str);
 }
 
-ssize_t	gnl_newline_index(const char *stor)
+ssize_t	gnl_newline_index(const char *store)
 {
 	ssize_t	i;
 
-	if (!stor)
+	if (!store)
 		return (-1);
 	i = 0;
-	while (stor[i])
+	while (store[i])
 	{
-		if (stor[i] == '\n')
+		if (store[i] == '\n')
 			return (i);
 		i++;
 	}
 	return (-1);
 }
 
-char	*gnl_clip_line(const char *stor)
+char	*gnl_clip_line(char *store)
 {
 	ssize_t	len;
 	size_t	i;
 	size_t	cpy_len;
 	char	*clip;
 
-	if (!stor)
+	if (!store)
 		return (NULL);
-	len = gnl_newline_index(stor);
+	len = gnl_newline_index(store);
 	cpy_len = 0;
 	if (len >= 0)
 		cpy_len = len + 1;
 	else
-		cpy_len = gnl_strlen(stor);
+		cpy_len = gnl_strlen(store);
 	clip = malloc(cpy_len + 1);
 	if (!clip)
-		return (free(stor), NULL);
+		return (free(store), NULL);
 	i = 0;
 	while (i < cpy_len)
 	{
-		clip[i] = stor[i];
+		clip[i] = store[i];
 		i++;
 	}
 	clip[i] = '\0';
 	return (clip);
 }
 
-char	*gnl_remain_re(char *stor)
+char	*gnl_get_remainder(char *store)
 {
 	ssize_t	idx;
 	size_t	len;
@@ -102,22 +102,22 @@ char	*gnl_remain_re(char *stor)
 	size_t	i;
 	char	*clip;
 
-	if (!stor)
+	if (!store)
 		return (NULL);
-	idx = gnl_newline_index(stor);
+	idx = gnl_newline_index(store);
 	if (idx < 0)
-		return (free(stor), NULL);
-	len = gnl_strlen(stor);
+		return (free(store), NULL);
+	len = gnl_strlen(store);
 	rem_len = len - (idx + 1);
 	if (rem_len == 0)
-		return (free(stor), NULL);
+		return (free(store), NULL);
 	clip = malloc(rem_len + 1);
 	if (!clip)
-		return (free(stor), NULL);
+		return (free(store), NULL);
 	i = 0;
 	while (i < rem_len)
-		clip[i] = stor[(idx + 1) + i++];
+		clip[i] = store[(idx + 1) + i++];
 	clip[rem_len] = '\0';
-	free(stor);
+	free(store);
 	return (clip);
 }
